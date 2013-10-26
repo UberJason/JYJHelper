652b4a8660326dec53808fa4bd3d579044bb988b
Jason Ji <Jason@jyj-air.home>
2013-10-25 23:44:03 -0400

    Initial Commit

---
+++ JYJ Helper/JYJAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JYJAppDelegate.h
+//  JYJ Helper
+//
+//  Created by Jason Ji on 10/25/13.
+//  Copyright (c) 2013 Jason Ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JYJAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JYJ Helper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  JYJ Helper
+//
+//  Created by Jason Ji on 10/25/13.
+//  Copyright (c) 2013 Jason Ji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JYJAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JYJAppDelegate class]));
+    }
+}
